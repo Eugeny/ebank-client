9dbee8eb21826e76bdb8d92f0137ce110b923813
Nikita Klyushnikov <vipkonst@gmail.com>
2014-10-28 23:08:13 +0300

    Added unstyled user accounts page

--- static/main/scripts/app/accounts/controllers/accountsCtrl.js
+++ static/main/scripts/app/accounts/controllers/accountsCtrl.js
@@ -1,4 +1,17 @@
 angular.module('ebank-client')
-    .controller('accountsCtrl', ['$scope',
-        function($scope) {
+    .controller('accountsCtrl', ['$scope', 'userAccountsService',
+        function($scope, userAccountsService) {
+            $scope.accounts = [];
+            $scope.isBusy = true;
+            $scope.stateTimestamp = new Date();
+
+            userAccountsService.getAccounts()
+                .then(function(accounts) {
+                    $scope.stateTimestamp = new Date();
+                    $scope.accounts = accounts;
+                }, function(error) {
+                    console.log(error);
+                }).finally(function() {
+                    $scope.isBusy = false;
+                });
         }]);
