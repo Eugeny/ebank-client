ca6ccf4647535538583d9e226f226221943fc461
Nikita Klushnikov <vipkonst@gmail.com>
2014-10-29 08:11:24 +0000

    Added accounts page refresh logic

--- static/main/scripts/app/accounts/controllers/accountsCtrl.js
+++ static/main/scripts/app/accounts/controllers/accountsCtrl.js
@@ -1,17 +1,27 @@
 angular.module('ebank-client')
     .controller('accountsCtrl', ['$scope', 'userAccountsService',
         function($scope, userAccountsService) {
+            function activate() {
+                $scope.reloadAccontsInformation()
+            }
+
             $scope.accounts = [];
             $scope.isBusy = true;
             $scope.stateTimestamp = new Date();
 
-            userAccountsService.getAccounts()
-                .then(function(accounts) {
-                    $scope.stateTimestamp = new Date();
-                    $scope.accounts = accounts;
-                }, function(error) {
-                    console.log(error);
-                }).finally(function() {
-                    $scope.isBusy = false;
-                });
+            $scope.reloadAccontsInformation = function() {
+                $scope.isBusy = true;
+
+                userAccountsService.getAccounts()
+                    .then(function(accounts) {
+                        $scope.stateTimestamp = new Date();
+                        $scope.accounts = accounts;
+                    }, function(error) {
+                        console.log(error);
+                    }).finally(function() {
+                        $scope.isBusy = false;
+                    });
+            };
+
+            activate();
         }]);
