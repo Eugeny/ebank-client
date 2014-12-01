8acbe168379302641f865b0c133d997f5b2dd468
Nikita Klyushnikov <vipkonst@gmail.com>
2014-12-01 22:37:12 +0300

    Added length regular expression

--- static/main/scripts/app/constants/validation-regular-expressions/validationRegularExpressionsConstant.js
+++ static/main/scripts/app/constants/validation-regular-expressions/validationRegularExpressionsConstant.js
@@ -4,5 +4,6 @@ angular.module('constants')
         password: /^[a-zA-Z0-9]{6,20}$/,
         passwordLength: /^.{6,20}$/,
         passwordCharacters: /^[a-zA-Z0-9]+$/,
-        integerNumberRegularExpression: /^[1-9][0-9]*$/
+        integerNumberRegularExpression: /^[1-9][0-9]*$/,
+        maxLength: /^.{0,1000}$/
     });
